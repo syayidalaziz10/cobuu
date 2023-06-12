cc8f036422cf27f8e2ee29819b764b2778096f34
syayidalaziz10 <shorashiro15@gmail.com>
2023-06-13 01:51:48 +0700

    color template

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,8 +6,20 @@ export default {
     "./resources/**/*.vue",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary':'#8FD2A7 !important',
+        'secondary' : '#E3F3E8 !important',
+        'content' : '#F4F6F8  !important',
+        'caption' : '#353535 !important',
+        'fill' : '#FFFFFF !important',
+        'color-1' : '#52B69A !important',
+        'color-2' : '#34A0A3 !important',
+        'color-3' : '#158AAD !important',
+      },
+    },
   },
   plugins: [],
 }
+
 
